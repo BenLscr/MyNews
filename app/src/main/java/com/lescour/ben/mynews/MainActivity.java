fe7e11c8431d4ac833c8278169310cc2a9c94b26
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-01-06 21:44:45 +0100

    Button on menu are selectable

--- app/src/main/java/com/lescour/ben/mynews/MainActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,5 +25,19 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_activity_main_search:
+                Toast.makeText(this, "Bouton 'rechercher' non assigné", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_activity_main_settings:
+                Toast.makeText(this, "Bouton 'parametre' non assigné", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
