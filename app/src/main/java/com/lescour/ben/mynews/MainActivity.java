43a452ca03b4251ec7c9455fd3ccb90d3459ed33
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-01-06 21:18:25 +0100

    Add a toolbar

--- app/src/main/java/com/lescour/ben/mynews/MainActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/MainActivity.java
@@ -2,6 +2,8 @@ package com.lescour.ben.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
     }
 }
