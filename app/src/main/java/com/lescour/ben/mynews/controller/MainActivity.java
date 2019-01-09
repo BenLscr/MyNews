9f8d4362d8b0d2ac3998674577d83bd7a2e00bb4
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-01-09 13:20:18 +0100

    Add a functional Navigation Drawer

--- app/src/main/java/com/lescour/ben/mynews/controller/MainActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/MainActivity.java
@@ -1,14 +1,19 @@
 package com.lescour.ben.mynews.controller;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
 import androidx.viewpager.widget.ViewPager;
 
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.tabs.TabLayout;
 import com.lescour.ben.mynews.R;
 import com.lescour.ben.mynews.controller.adapter.PageAdapter;
@@ -18,7 +23,21 @@ import com.lescour.ben.mynews.controller.fragment.dummy.DummyContent;
 
 public class MainActivity extends AppCompatActivity implements
         TopStoriesFragment.OnListFragmentInteractionListener,
-        MostPopularFragment.OnListFragmentInteractionListener {
+        MostPopularFragment.OnListFragmentInteractionListener,
+        NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private NavigationView navigationView;
+    private ViewPager viewPager;
+
+    private Fragment fragmentTopStories;
+    private Fragment fragmentMostPopular;
+
+
+    private static final int FRAGMENT_TOP_STORIES = 0;
+    private static final int FRAGMENT_MOST_POPULAR = 1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +45,12 @@ public class MainActivity extends AppCompatActivity implements
         setContentView(R.layout.activity_main);
         this.configureToolbar();
         this.configureViewPagerAndTabs();
+        this.configureDrawerLayout();
+        this.configureNavigationView();
     }
 
     private void configureToolbar() {
-        Toolbar toolbar = findViewById(R.id.activity_main_toolbar);
+        this.toolbar = findViewById(R.id.activity_main_toolbar);
         setSupportActionBar(toolbar);
     }
 
@@ -60,18 +81,64 @@ public class MainActivity extends AppCompatActivity implements
     }
 
     private void configureViewPagerAndTabs(){
-        // 1 - Get ViewPager from layout
-        ViewPager pager = (ViewPager)findViewById(R.id.activity_main_viewpager);
+        this.viewPager = (ViewPager)findViewById(R.id.activity_main_viewpager);
         // 2 - Set Adapter PageAdapter and glue it together
-        pager.setAdapter(new PageAdapter(getSupportFragmentManager()) {
+        viewPager.setAdapter(new PageAdapter(getSupportFragmentManager()) {
         });
         TabLayout tabs = (TabLayout) findViewById(R.id.activity_main_tabs);
-        tabs.setupWithViewPager(pager);
+        tabs.setupWithViewPager(viewPager);
         tabs.setTabMode(TabLayout.MODE_FIXED);
     }
 
     @Override
     public void onListFragmentInteraction(DummyContent.DummyItem item) {
+    }
 
+
+//////////    MENU    //////////
+
+    private void configureDrawerLayout(){
+        this.drawerLayout = (DrawerLayout) findViewById(R.id.activity_main_drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+    private void configureNavigationView(){
+        this.navigationView = (NavigationView) findViewById(R.id.activity_main_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+
+    /**
+     * When a item is selected. Display the associate fragment in the ViewPager.
+     */
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id){
+            case R.id.activity_main_drawer_top_stories :
+                viewPager.setCurrentItem(0);
+                break;
+            case R.id.activity_main_drawer_most_popular:
+                viewPager.setCurrentItem(1);
+                break;
+            default:
+                break;
+        }
+
+        this.drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
+    }
+
+    /**
+     * Close the NavigationDrawer with the button back
+     */
+    @Override
+    public void onBackPressed() {
+        if (this.drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            this.drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
