762ab1964f27afffc8102f5684c3bee78e80acc8
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-01-12 15:48:29 +0100

    Add dependencies : retrofit & gson

--- app/src/androidTest/java/com/lescour/ben/mynews/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/lescour/ben/mynews/ExampleInstrumentedTest.java
@@ -1,15 +1,11 @@
 package com.lescour.ben.mynews;
 
 import android.content.Context;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Test;
 
 import androidx.test.InstrumentationRegistry;
 
-import static androidx.test.espresso.Espresso.onView;
-import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -25,15 +21,5 @@ public class ExampleInstrumentedTest {
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.lescour.ben.mynews", appContext.getPackageName());
-    }
-
-    @Test
-    @LargeTest
-    public void checkMostPopularFragment() {
-        onView(withId(R.id.activity_main_nav_view)).perform(click());
-
-        onView(withId(R.id.activity_main_drawer_most_popular)).perform(click());
-
-        //onView(withId()).check(matches(isDisplayed()));
     }
 }
