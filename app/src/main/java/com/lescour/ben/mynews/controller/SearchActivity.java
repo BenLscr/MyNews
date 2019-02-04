c47f1e0a753e838bddda783c6ff2419259245b7a
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-04 13:34:25 +0100

    Add back button to SearchActivity

--- app/src/main/java/com/lescour/ben/mynews/controller/SearchActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/SearchActivity.java
@@ -4,18 +4,36 @@ import android.os.Bundle;
 
 import com.lescour.ben.mynews.R;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benja on 04/02/2019.
  */
 public class SearchActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.search_activity);
+        ButterKnife.bind(this);
+        this.configureToolbar();
     }
 
+    private void configureToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
 }
