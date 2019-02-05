0d28359348a6c14145a02de88b70c6a16e69b1c2
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-05 22:35:09 +0100

    Add toolbar and change parent activity in the manifest (CustomActivity

--- app/src/main/java/com/lescour/ben/mynews/controller/CustomActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/CustomActivity.java
@@ -5,16 +5,31 @@ import android.os.Bundle;
 import com.lescour.ben.mynews.R;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benja on 05/02/2019.
  */
 public class CustomActivity extends AppCompatActivity {
+    @BindView(R.id.activity_main_toolbar)
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.custom_activity);
+        ButterKnife.bind(this);
+
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
     }
 }
