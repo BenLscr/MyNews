76092d5a1c3a5c23599d9a2458baf082bc5191a1
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-11 23:50:40 +0100

    Configure toolbar. (NotificationsActivity)

--- app/src/main/java/com/lescour/ben/mynews/controller/NotificationsActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/NotificationsActivity.java
@@ -4,16 +4,37 @@ import android.os.Bundle;
 
 import com.lescour.ben.mynews.R;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benja on 11/02/2019.
  */
 public class NotificationsActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notifications_activity);
+        ButterKnife.bind(this);
+        this.configureToolbar();
+    }
+
+    private void configureToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
