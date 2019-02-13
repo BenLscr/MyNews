2817d80a94aaff78eade234591dc497c621eaa24
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-13 23:19:07 +0100

    Add toolbar. (HelpActivity)

--- app/src/main/java/com/lescour/ben/mynews/controller/HelpActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/HelpActivity.java
@@ -4,17 +4,38 @@ import android.os.Bundle;
 
 import com.lescour.ben.mynews.R;
 
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benja on 13/02/2019.
  */
 public class HelpActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_toolbar) Toolbar toolbar;
+
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help_activity);
+        ButterKnife.bind(this);
+        this.configureToolbar();
+    }
+
+//////////    Toolbar   //////////
+
+    protected void configureToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
     }
 }
