b9ca8102e1ec465e4cb3c5d35f4bb17e0b16df72
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-13 23:28:59 +0100

    Add toolbar. (AboutActivity)

--- app/src/main/java/com/lescour/ben/mynews/controller/AboutActivity.java
+++ app/src/main/java/com/lescour/ben/mynews/controller/AboutActivity.java
@@ -4,16 +4,39 @@ import android.os.Bundle;
 
 import com.lescour.ben.mynews.R;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by benja on 13/02/2019.
  */
 public class AboutActivity extends AppCompatActivity {
 
+    @BindView(R.id.activity_main_toolbar) Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about_activity);
+        ButterKnife.bind(this);
+        this.configureToolbar();
     }
+
+//////////    Toolbar   //////////
+
+    protected void configureToolbar() {
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
 }
