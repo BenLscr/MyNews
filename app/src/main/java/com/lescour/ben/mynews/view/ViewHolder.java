b562a47c0324d78e020463fd76c6ae04b774794a
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-25 23:31:25 +0100

    Clean code. (ViewHolder)

--- app/src/main/java/com/lescour/ben/mynews/view/ViewHolder.java
+++ app/src/main/java/com/lescour/ben/mynews/view/ViewHolder.java
@@ -7,6 +7,7 @@ import android.widget.TextView;
 import com.lescour.ben.mynews.R;
 import com.lescour.ben.mynews.model.Article;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -15,19 +16,20 @@ import butterknife.ButterKnife;
  * Created by benja on 10/02/2019.
  */
 public class ViewHolder extends RecyclerView.ViewHolder {
-    public final View mView;
+    final View mView;
     public @BindView(R.id.img_article) ImageView articleImg;
     public @BindView(R.id.article_sectionSubsection) TextView articleSectionSubsection;
     public @BindView(R.id.article_date) TextView articleDate;
     public @BindView(R.id.article_title) TextView articleTitle;
     public Article article;
 
-    public ViewHolder(View view) {
+    ViewHolder(View view) {
         super(view);
         mView = view;
         ButterKnife.bind(this, view);
     }
 
+    @NonNull
     @Override
     public String toString() {
         return super.toString() + " '" + articleDate.getText() + "'";
