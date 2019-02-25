21dcf05231977256d8903c725ac65efaa3a21465
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-25 13:08:27 +0100

    Update beginDateNeedYesterday(). (IntegrationTest)

--- app/src/test/java/com/lescour/ben/mynews/IntegrationTest.java
+++ app/src/test/java/com/lescour/ben/mynews/IntegrationTest.java
@@ -1,5 +1,7 @@
 package com.lescour.ben.mynews;
 
+import android.content.Context;
+
 import com.lescour.ben.mynews.controller.CustomActivity;
 import com.lescour.ben.mynews.model.UrlSplit;
 import com.lescour.ben.mynews.utils.NotificationsWorker;
@@ -11,26 +13,36 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Locale;
 
+import androidx.work.WorkerParameters;
+
 import static org.junit.Assert.assertEquals;
-import static org.mockito.ArgumentMatchers.matches;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by benja on 19/02/2019.
  */
 public class IntegrationTest {
 
-    /**@Test
+    private UrlSplit mUrlSplit;
+    private Calendar calendar;
+
+    @Before
+    public void setUp(){
+        mUrlSplit = new UrlSplit();
+        //mUrlSplit.setQuery("Trump");
+        mUrlSplit.setFilter_query("news_desk:(\"business\")");
+
+        calendar = Calendar.getInstance();
+    }
+
+    @Test
     public void beginDateNeedYesterday() {
-    NotificationsWorker notificationsWorker = mock(NotificationsWorker.class);
+        NotificationsWorker notificationsWorker = new NotificationsWorker(mock(Context.class), mock(WorkerParameters.class));
 
-    Calendar calendar = Calendar.getInstance();
-
-    Calendar calendar2 = Calendar.getInstance();
-    calendar2.add(Calendar.DAY_OF_MONTH, -1);
-    SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
-    String beginDate = format.format(calendar2.getTime());
+        Calendar calendar2 = Calendar.getInstance();
+        calendar2.add(Calendar.DAY_OF_MONTH, -1);
+        SimpleDateFormat format = new SimpleDateFormat("yyyyMMdd", Locale.getDefault());
+        String beginDate = format.format(calendar2.getTime());
 
     assertEquals(beginDate, notificationsWorker.setYesterdayToBeginDate(calendar));
     }*/
