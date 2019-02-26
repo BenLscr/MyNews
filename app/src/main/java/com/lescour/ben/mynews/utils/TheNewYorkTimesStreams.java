902dd0c1ac779c12910cdadc71ff98c007f64a1a
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-02-26 15:47:42 +0100

    Add javadoc. (TheNewYorkTimesStreams)

--- app/src/main/java/com/lescour/ben/mynews/utils/TheNewYorkTimesStreams.java
+++ app/src/main/java/com/lescour/ben/mynews/utils/TheNewYorkTimesStreams.java
@@ -13,6 +13,14 @@ import io.reactivex.schedulers.Schedulers;
  */
 public class TheNewYorkTimesStreams {
 
+    /**
+     * An observable representing a data stream -for TopStories- that allows subscribers to
+     * register and retrieve their data emissions.
+     * subscribeOn() : Allow to execute the observable in a dedicated Thread.
+     * observeOn() : Allow all subscribers to listen to the observable data stream on the Main Thread.
+     * timeout() : If no data is emitted before the set time (here 10 seconds), the data emission will
+     * be cut and a timeout error will be sent to Subscriber(s).
+     */
     public static Observable<TheNewYorkTimesResponse> streamFetchTopStories(String section, String apiKey){
         TheNewYorkTimesService theNewYorkTimesService = TheNewYorkTimesService.retrofit.create(TheNewYorkTimesService.class);
         return theNewYorkTimesService.getTopStories(section, apiKey)
@@ -21,6 +29,14 @@ public class TheNewYorkTimesStreams {
                 .timeout(10, TimeUnit.SECONDS);
     }
 
+    /**
+     * An observable representing a data stream -for MostPopular- that allows subscribers to
+     * register and retrieve their data emissions.
+     * subscribeOn() : Allow to execute the observable in a dedicated Thread.
+     * observeOn() : Allow all subscribers to listen to the observable data stream on the Main Thread.
+     * timeout() : If no data is emitted before the set time (here 10 seconds), the data emission will
+     * be cut and a timeout error will be sent to Subscriber(s).
+     */
     public static Observable<TheNewYorkTimesResponse> streamFetchMostPopular(String period, String apiKey){
         TheNewYorkTimesService theNewYorkTimesService = TheNewYorkTimesService.retrofit.create(TheNewYorkTimesService.class);
         return theNewYorkTimesService.getMostPopular(period, apiKey)
@@ -29,6 +45,14 @@ public class TheNewYorkTimesStreams {
                 .timeout(10, TimeUnit.SECONDS);
     }
 
+    /**
+     * An observable representing a data stream -for ArticleSearch- that allows subscribers to
+     * register and retrieve their data emissions.
+     * subscribeOn() : Allow to execute the observable in a dedicated Thread.
+     * observeOn() : Allow all subscribers to listen to the observable data stream on the Main Thread.
+     * timeout() : If no data is emitted before the set time (here 10 seconds), the data emission will
+     * be cut and a timeout error will be sent to Subscriber(s).
+     */
     public static Observable<TheNewYorkTimesResponse> streamFetchArticleSearch(String begin_date, String end_date, String filter_query, String query, String sort, String apiKey){
         TheNewYorkTimesService theNewYorkTimesService = TheNewYorkTimesService.retrofit.create(TheNewYorkTimesService.class);
         return theNewYorkTimesService.getArticleSearch(begin_date, end_date, filter_query, query, sort, apiKey)
