6f2eeb59dc3199667c20cbb92ab0e3cad7e49b0a
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-03-04 12:56:25 +0100

    Add a test to the search form. When the form is valid. (InstrumentedTest)

---
+++ app/src/androidTest/java/com/lescour/ben/mynews/InstrumentedTest.java
@@ -0,0 +1,40 @@
+package com.lescour.ben.mynews;
+
+import com.lescour.ben.mynews.controller.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import androidx.test.rule.ActivityTestRule;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.pressBack;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+
+public class InstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+
+    @Test
+    public void searchTool_ValidForm(){
+    onView(withId(R.id.menu_activity_main_search)).perform(click());
+
+    onView(withId(R.id.search_query_term)).perform(typeText("Trump"));
+    onView(withId(R.id.search_query_term)).perform(pressBack());
+    onView(withId(R.id.checkbox_politics)).perform(click());
+
+    onView(withId(R.id.launch_search_button)).perform(click());
+    onView(withId(R.id.custom_activity)).check(matches(isDisplayed()));
+    }
+}
