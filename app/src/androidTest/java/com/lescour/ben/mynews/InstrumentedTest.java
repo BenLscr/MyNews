5be8d50ee00519d14f97c8b5a21cb9cbe419c957
LESCOUR Benjamin <benjamin76510@hotmail.fr>
2019-03-05 13:32:37 +0100

    Add a test tryAOtherCategory_InMenuDrawer().  (InstrumentedTest)

--- app/src/androidTest/java/com/lescour/ben/mynews/InstrumentedTest.java
+++ app/src/androidTest/java/com/lescour/ben/mynews/InstrumentedTest.java
@@ -1,50 +1,91 @@
 package com.lescour.ben.mynews;
 
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewParent;
+
 import com.lescour.ben.mynews.controller.MainActivity;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import androidx.test.espresso.ViewInteraction;
 import androidx.test.rule.ActivityTestRule;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
+import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-
 @RunWith(JUnit4.class)
 public class InstrumentedTest {
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityTestRule = new ActivityTestRule<>(MainActivity.class);
 
-    /*@Test
-    public void searchTool_ValidForm() {
-    onView(withId(R.id.menu_activity_main_search)).perform(click());
-
-    onView(withId(R.id.search_query_term)).perform(typeText("Trump"));
-    onView(withId(R.id.search_query_term)).perform(closeSoftKeyboard());
-    onView(withId(R.id.checkbox_politics)).perform(click());
-
-    onView(withId(R.id.launch_search_button)).perform(click());
-    onView(withId(R.id.custom_activity)).check(matches(isDisplayed()));
-    }*/
-
+    /**
+     * Start from the home screen, open the menu drawer and click on Business(from Other categories)
+     * and open a new view (custom_view) and check if a list of article is displayed.
+     */
     @Test
-    public void search_button() {
-        onView(withId(R.id.menu_activity_main_search)).perform(click());
-        onView(withId(R.id.search_activity)).check(matches(isDisplayed()));
+    public void tryAOtherCategory_InMenuDrawer() {
+        ViewInteraction appCompatImageButton = onView(
+                allOf(withContentDescription("Open navigation drawer"),
+                        childAtPosition(
+                                allOf(withId(R.id.activity_main_toolbar),
+                                        childAtPosition(
+                                                withClassName(is("android.widget.LinearLayout")),
+                                                0)),
+                                1),
+                        isDisplayed()));
+        appCompatImageButton.perform(click());
+
+        ViewInteraction navigationMenuItemView = onView(
+                allOf(childAtPosition(
+                        allOf(withId(R.id.design_navigation_view),
+                                childAtPosition(
+                                        withId(R.id.activity_main_nav_view),
+                                        0)),
+                        8),
+                        isDisplayed()));
+        navigationMenuItemView.perform(click());
+
+        onView(withId(R.id.list)).check(matches(isDisplayed()));
+
+    }
+
+    private static Matcher<View> childAtPosition(
+            final Matcher<View> parentMatcher, final int position) {
+
+        return new TypeSafeMatcher<View>() {
+            @Override
+            public void describeTo(Description description) {
+                description.appendText("Child at position " + position + " in parent ");
+                parentMatcher.describeTo(description);
+            }
+
+            @Override
+            public boolean matchesSafely(View view) {
+                ViewParent parent = view.getParent();
+                return parent instanceof ViewGroup && parentMatcher.matches(parent)
+                        && view.equals(((ViewGroup) parent).getChildAt(position));
+            }
+        };
     }
 
 }
